201d20a9f4ae99bdfe985ac24b9ad7ffb8157771
Nick Bartlett <nickbartlett19@gmail.com>
2023-11-17 12:21:34 -0500

    clean up code

--- scripts/script.js
+++ scripts/script.js
@@ -18,25 +18,3 @@ function toggleMenu() {
 
 /* Event Listener */
 toggle.addEventListener("click", toggleMenu, false);
-
-// const items = document.querySelectorAll(".item");
-
-// /* Activate Submenu */
-// function toggleItem() {
-//   if (this.classList.contains("submenu-active")) {
-//     this.classList.remove("submenu-active");
-//   } else if (menu.querySelector(".submenu-active")) {
-//     menu.querySelector(".submenu-active").classList.remove("submenu-active");
-//     this.classList.add("submenu-active");
-//   } else {
-//     this.classList.add("submenu-active");
-//   }
-// }
-
-// /* Event Listeners */
-// for (let item of items) {
-//     if (item.querySelector(".submenu")) {
-//       item.addEventListener("click", toggleItem, false);
-//       item.addEventListener("keypress", toggleItem, false);
-//     }   
-// }
