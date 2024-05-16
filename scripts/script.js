bf05f5b2e1095216968563489eeaf29761e0979f
Nick Bartlett <nickbartlett19@gmail.com>
2024-05-15 21:56:50 -0400

    toggleMenu() edits

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,16 @@ const menu = document.querySelector(".menu");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
+    if (menu.classList.contains("hide")) {
+        menu.classList.remove("hide");
         // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-    } else {
-        menu.classList.add("active");
-
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+        toggle.innerHTML = "<i class='fas fa-bars'></i>";
+    } 
+    else {
+        menu.classList.add("hide");
+        toggle.innerHTML = "<i class='fas fa-times'></i>";
     }
 }
 
 /* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
+toggle.addEventListener("click", toggleMenu);
